aca879d3add954d2d9e444115b1f5d2656adc01f
Anna Senina <as.senina@gmail.com>
2020-09-10 08:40:12 +0300

    Simple selenide search test

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,32 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+@Epic("Q.A. Guru automation course")
+@Story("HW 12")
+@Tag("Google")
+public class SimpleTest {
+
+    @Test
+    @Description("Google test, we look for Selenide")
+    @DisplayName("Succsessful search for selenide in google")
+    void selenideSearchTest() {
+        // Открыть google
+        open("https://google.com");
+
+        // Ввести Selenide в поиск
+        $(byName("q")).setValue("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("html").shouldHave(text("ru.selenide.org"));
+    }
+}
+
