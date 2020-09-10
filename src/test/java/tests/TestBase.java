d417832a8d701ed72c600f276000796de3d57eff
Anna Senina <as.senina@gmail.com>
2020-09-10 10:17:58 +0300

    addRemoteDriver

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,8 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import drivers.CustomWebDriver;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -18,6 +20,7 @@ public class TestBase {
     @Step("Tests setup")
     public static void beforeall() {
         addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.browser = CustomWebDriver.class.getName();
     }
 
     @AfterEach
